3952ff11e76279fb7de442ff0bafe5f8c2827442
Arran France <arran@arranfrance.com>
2021-06-13 00:20:47 +0100

    tidied up version

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-use embedded_graphics::{
-    image::ImageRaw,
-    pixelcolor::{raw::BigEndian, BinaryColor},
-    prelude::*,
-};
-use epd_waveshare::{
-    epd7in5_v2::{Display7in5, EPD7in5},
-    graphics::Display,
-    prelude::*,
-};
-use rand::seq::SliceRandom;
 use std::{
     ffi::OsStr,
     fs::{self, File},
@@ -17,21 +6,34 @@ use std::{
 };
 
 use embedded_graphics::image::Image;
-
+use embedded_graphics::{
+    image::ImageRaw,
+    pixelcolor::{raw::BigEndian, BinaryColor},
+    prelude::*,
+};
+use epd_waveshare::{
+    epd7in5_v2::{Display7in5, Epd7in5},
+    graphics::Display,
+    prelude::*,
+};
 use rppal::gpio::Gpio;
 use rppal::hal::Delay;
 use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
 
 use anyhow::{Context, Result};
+use rand::seq::SliceRandom;
+
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 480;
+const IMAGE_EXTENSION: &str = "txt";
 
 fn main() -> Result<()> {
-    let (mut spi, mut epd7in5) = setup_waveshare();
-
+    let (mut spi, mut epd7in5, mut delay) =
+        setup_waveshare().with_context(|| "Failed to initialise waveshare display")?;
     let mut display = Display7in5::default();
 
     let data = get_random_image().with_context(|| "Failed to get random image")?;
-    let raw_image = ImageRaw::<BinaryColor, BigEndian>::new(&data, 800, 480);
-
+    let raw_image = ImageRaw::<BinaryColor, BigEndian>::new(&data, WIDTH, HEIGHT);
     let image = Image::new(&raw_image, Point::zero());
 
     image
@@ -39,14 +41,16 @@ fn main() -> Result<()> {
         .with_context(|| "Failed to draw to screen")?;
 
     epd7in5
-        .update_frame(&mut spi, &display.buffer())
+        .update_frame(&mut spi, &display.buffer(), &mut delay)
         .with_context(|| "Failed to update frame")?;
     epd7in5
-        .display_frame(&mut spi)
+        .display_frame(&mut spi, &mut delay)
         .with_context(|| "Failed to display frame")?;
 
     println!("Finished rendering - going to sleep");
-    epd7in5.sleep(&mut spi).with_context(|| "Failed to sleep")?;
+    epd7in5
+        .sleep(&mut spi, &mut delay)
+        .with_context(|| "Failed to sleep")?;
     Ok(())
 }
 
@@ -58,7 +62,7 @@ fn get_random_image() -> Result<Vec<u8>> {
             let path = entry.path();
             let extension = path.extension().and_then(OsStr::to_str);
             match extension {
-                Some(extension) => extension == "txt",
+                Some(extension) => extension == IMAGE_EXTENSION,
                 None => false,
             }
         })
@@ -75,16 +79,18 @@ fn get_random_image() -> Result<Vec<u8>> {
     Ok(data)
 }
 
-fn setup_waveshare() -> (
+fn setup_waveshare() -> Result<(
     Spi,
-    EPD7in5<
+    Epd7in5<
         Spi,
         rppal::gpio::OutputPin,
         rppal::gpio::OutputPin,
         rppal::gpio::OutputPin,
         rppal::gpio::OutputPin,
+        Delay,
     >,
-) {
+    Delay,
+)> {
     // activate spi, gpio in raspi-config
     // needs to be run with sudo because of some sysfs_gpio permission problems and follow-up timing problems
     // see https://github.com/rust-embedded/rust-sysfs-gpio/issues/5 and follow-up issues
@@ -92,39 +98,41 @@ fn setup_waveshare() -> (
     // This code matches the pins described in https://www.waveshare.com/wiki/7.5inch_e-Paper_HAT
     // It also matches the code from https://github.com/waveshare/e-Paper
     let mut spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 4_000_000, Mode::Mode0)
-        .expect("Unable to configure SPI");
-    spi.set_bits_per_word(8).expect("Set bits per word");
+        .with_context(|| "Unable to configure SPI")?;
+    spi.set_bits_per_word(8)
+        .with_context(|| "Set bits per word")?;
 
     let mut rst = Gpio::new()
-        .expect("Failed to get GPIO")
+        .with_context(|| "Failed to get GPIO")?
         .get(17) // Board 11 BCM 17
-        .expect("Failed to get BCM Pin 17 for RST")
+        .with_context(|| "Failed to get BCM Pin 17 for RST")?
         .into_output();
     rst.set_low();
     rst.set_high();
 
     let mut dc = Gpio::new()
-        .expect("Failed to get GPIO")
+        .with_context(|| "Failed to get GPIO")?
         .get(25) //Board 22, BCM 25
-        .expect("Failed to get BCM Pin 25 for RST")
+        .with_context(|| "Failed to get BCM Pin 25 for RST")?
         .into_output();
     dc.set_low();
     dc.set_high();
 
     let mut cs = Gpio::new()
-        .expect("Failed to get GPIO")
+        .with_context(|| "Failed to get GPIO")?
         .get(8) //Board 24, BCM 8
-        .expect("Failed to get BCM Pin 8 for RST")
+        .with_context(|| "Failed to get BCM Pin 8 for RST")?
         .into_output();
     cs.set_high();
 
     let busy = Gpio::new()
-        .expect("Failed to get GPIO")
+        .with_context(|| "Failed to get GPIO")?
         .get(24) // Board 18, BCM 24
-        .expect("Failed to get BCM Pin 24 for RST")
+        .with_context(|| "Failed to get BCM Pin 24 for RST")?
         .into_output();
+
     let mut delay = Delay {};
-    let epd7in5 =
-        EPD7in5::new(&mut spi, cs, busy, dc, rst, &mut delay).expect("eink initalize error");
-    (spi, epd7in5)
+    let epd7in5 = Epd7in5::new(&mut spi, cs, busy, dc, rst, &mut delay)
+        .with_context(|| "eink initalize error")?;
+    Ok((spi, epd7in5, delay))
 }
